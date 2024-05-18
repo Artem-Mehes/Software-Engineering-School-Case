0045f13c38ba933b2cb520518f42d98dfdd8bc9e
Artem Mehes <artem.m@sweetcode.pro>
2024-05-18 11:36:04 +0300

    Setup database and save /subscribe email in database

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,21 @@ import express from "express";
 const app = express();
 const port = 3000;
 import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "../database.types";
 
 dotenv.config();
 
 import apiRouter from "./routes/api";
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRouter);
+
+const supabaseUrl = "https://abwaahqyxlhvfgykvduw.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey!);
 
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,21 +1,57 @@
 import { Router, Request, Response } from "express";
 import axios from "axios";
+import multer from "multer";
+import { supabase } from "../app";
+const upload = multer();
 
 const router = Router();
 
-router.get("/rate", async (_, res) => {
+router.get("/rate", async (_, res: Response<number | { error: string }>) => {
   try {
-    const response = await axios.get(
+    const response = await axios.get<{ conversion_rate: number }>(
       `${process.env.EXCHANGE_RATE_API_BASE_URL}/${process.env.EXCHANGE_RATE_API_KEY}/pair/USD/UAH`,
     );
+    /*TODO: types?*/
     res.json(response.data.conversion_rate);
   } catch (error) {
+    /*TODO: better handling?*/
     res.status(500).json({ error: "Failed to fetch currency rate" });
   }
 });
 
-router.get("/subscribe", (req: Request, res: Response) => {
-  res.send("Subscribe");
-});
+/*TODO: Test that it's email type*/
+router.post(
+  "/subscribe",
+  upload.none(),
+  async (req: Request<{ email: string }>, res) => {
+    const email = req.body.email;
+
+    if (!email) {
+      return res.status(400).json({ error: "Email is required" });
+    }
+
+    try {
+      const existingSubscription = await supabase
+        .from("subscriptions")
+        .select("value", email)
+        .single();
+
+      if (existingSubscription.data) {
+        return res
+          .status(409)
+          .json({ message: existingSubscription.data.value });
+      } else {
+        /*TODO: error*/
+        const { error } = await supabase
+          .from("subscriptions")
+          .insert([{ value: email }]);
+
+        return res.status(200).json({ message: "E-mail додано" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Failed to subscribe" });
+    }
+  },
+);
 
 export default router;
