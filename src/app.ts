3dc8d7faec185d6f6a22d979444ac6a4f642252c
Artem Mehes <artem.m@sweetcode.pro>
2024-05-18 10:25:37 +0300

    Add /rate route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from "express";
 const app = express();
 const port = 3000;
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import apiRouter from "./routes/api";
 
 app.use(express.json());
+
+app.use("/api", apiRouter);
 
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,9 +1,17 @@
 import { Router, Request, Response } from "express";
+import axios from "axios";
 
 const router = Router();
 
-router.get("/rate", (req: Request, res: Response) => {
-  res.send("Get rate");
+router.get("/rate", async (_, res) => {
+  try {
+    const response = await axios.get(
+      `${process.env.EXCHANGE_RATE_API_BASE_URL}/${process.env.EXCHANGE_RATE_API_KEY}/pair/USD/UAH`,
+    );
+    res.json(response.data.conversion_rate);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch currency rate" });
+  }
 });
 
 router.get("/subscribe", (req: Request, res: Response) => {
